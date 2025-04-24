0ae15ecb7b5afaac75ffa95736c4c9c2415cc48e
NimishaChavan <chavannaman480@gmail.com>
2025-04-24 13:22:55 +0530

    save

--- demo.java
+++ demo.java
@@ -2,7 +2,7 @@ class demo{
     public static void main(String[] args)
     {
         int a=10;
-        int b=20;
+        int b=10;
         int sum=a+b;
         int sub=a-b;
         System.out.println("sum is: "+sum);
